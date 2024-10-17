82781d063e6c69573c67eb92e6ac7a1a7023e186
Benjamin <benjamin@ekramiand@gmail.com>
2024-10-17 16:25:19 +0100

    revamp the custom hook

--- src/hooks/useMusicSearch.js
+++ src/hooks/useMusicSearch.js
@@ -1,23 +1,26 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { setTracks } from '../store/musicSlice';
+import { setSelectedMusic, setTracks } from '../store/musicSlice';
+import axios from 'axios';
 
 const useMusicSearch = () => {
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [success, setSuccess] = useState(false);
+    const url = 'https://itunes.apple.com';
 
-    const searchMusic = async (searchTxt) => {
+    const searchMusic = async (searchTxt = 'pop') => {
         setLoading(true);
         setError(null);
         setSuccess(false);
 
         try {
-            const response = await fetch(`https://itunes.apple.com/search?term=${searchTxt}`);
-            const data = await response.json();
-            if (data.results && data.results.length > 0) {
-                dispatch(setTracks(data.results));
+
+            const response = await axios.get(`${url}/search?term=${searchTxt}`);
+            const results = response.data.results;
+            if (results && results.length > 0) {
+                dispatch(setTracks(results));
                 setSuccess(true); // Set success to true when results are found
             } else {
                 setError('No results found.'); // Handle no results scenario
@@ -30,7 +33,25 @@ const useMusicSearch = () => {
         }
     };
 
-    return { searchMusic, loading, error, success };
+    const findMusicById = async (id) => {
+        try {
+            const response = await axios.get(`${url}/lookup?id=${id}`);
+            if (response.data.results.length > 0) {
+                const track = response.data.results[0];
+                dispatch(setSelectedMusic(track));
+                setSuccess(true); // Set success to true when results are found
+            } else {
+                setError('No results found.'); // Handle no results scenario
+            }
+        } catch (error) {
+            console.error('Error fetching music:', error);
+            setError('Failed to fetch music. Please try again.'); // Set error message
+        } finally {
+            setLoading(false); // Reset loading state
+        }
+    }
+
+    return { searchMusic, findMusicById, loading, error, success };
 };
 
 export default useMusicSearch;
