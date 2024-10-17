613ccf4361b047d315ef7e0604710db6e7b6eae2
Benjamin <benjamin@ekramiand@gmail.com>
2024-10-17 16:25:24 +0100

    add scrolling pagination to MusicList and throttling  it

--- src/components/MusicDetail/MusicDetail.jsx
+++ src/components/MusicDetail/MusicDetail.jsx
@@ -1,37 +1,73 @@
-// src/components/MusicDetail.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import useMusicSearch from '../../hooks/useMusicSearch';
 
 const MusicDetail = () => {
     const { id } = useParams();
-    const results = useSelector((state) => state.music.searchResults);
-    const music = results.find((track) => track.trackId === parseInt(id));
+    const [music, setMusic] = useState(null);
+    const { findMusicById, loading, error, success } = useMusicSearch();
+    const { results, selectedMusic } = useSelector((state) => state.music);
+
+    useEffect(() => {
+        const fetchMusic = async () => {
+            // check memory first
+            if (results.length > 0) {
+                const track = results.find((track) => track.trackId === parseInt(id));
+                localStorage.setItem(`music_track`, JSON.stringify(track)); // Save to local storage
+                setMusic(track)
+                return;
+            }
+
+            // Check local storage 
+            const cachedMusic = localStorage.getItem(`music_track`);
+            if (cachedMusic) {
+                setMusic(JSON.parse(cachedMusic)); // Set music from local storage
+                return
+            }
+
+            // if data is not cached then request iTunes endpoint
+            findMusicById(id)
+
+        };
+
+        fetchMusic();
+    }, [id]);
+
+    useEffect(() => {
+        if (success) {
+            setMusic(selectedMusic)
+        }
+    }, [success]);
 
     if (!music) {
         return <div className="text-center p-4">Loading...</div>;
     }
 
     return (
-        <div className="max-w-2xl mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">{music.trackName}</h1>
+        <div className="max-w-2xl mx-auto p-6 bg-gradient-to-r from-purple-500 to-indigo-500 rounded-lg shadow-lg text-white mt-6">
+            <h1 className="text-3xl font-extrabold mb-4 text-center">{music.trackName}</h1>
             <img
                 src={music.artworkUrl100}
                 alt={music.trackName}
-                className="rounded shadow-md mb-4"
+                className="rounded-lg shadow-md mb-4 mx-auto transition-transform transform hover:scale-105"
             />
-            <p className="text-lg mb-2"><strong>Artist:</strong> {music.artistName}</p>
-            <p className="text-lg mb-2"><strong>Album:</strong> {music.collectionName}</p>
-            <p className="text-lg mb-2"><strong>Release Date:</strong> {music.releaseDate}</p>
-            <p className="text-lg mb-2"><strong>Genre:</strong> {music.primaryGenreName}</p>
-            <a
-                href={music.trackViewUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
-            >
-                Listen on iTunes
-            </a>
+            <div className="mb-4 text-lg">
+                <p className="mb-2"><strong>Artist:</strong> {music.artistName}</p>
+                <p className="mb-2"><strong>Album:</strong> {music.collectionName}</p>
+                <p className="mb-2"><strong>Release Date:</strong> {new Date(music.releaseDate).toLocaleDateString()}</p>
+                <p className="mb-2"><strong>Genre:</strong> {music.primaryGenreName}</p>
+            </div>
+            <div className="text-center">
+                <a
+                    href={music.trackViewUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-yellow-400 text-black font-semibold px-6 py-2 rounded-lg shadow-md hover:bg-yellow-500 transition duration-300"
+                >
+                    Listen on iTunes
+                </a>
+            </div>
         </div>
     );
 };
--- src/components/MusicList/MusicList.jsx
+++ src/components/MusicList/MusicList.jsx
@@ -1,30 +1,137 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { MusicCard } from './../';
-import useMusicSearch from './../../hooks/useMusicSearch';
+import { MusicCard } from '..';
+import useMusicSearch from '../../hooks/useMusicSearch';
 
+
+const throttle = (func, limit) => {
+    let lastFunc;
+    let lastRan;
+
+    return function (...args) {
+        const context = this;
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(() => {
+                if (Date.now() - lastRan >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+};
+
+
+// since the iTune api is not providing pagination structure so I used in memory pagination for scrolling
+const pageSize = 12;
 const MusicList = () => {
     const { searchMusic, loading, error } = useMusicSearch();
-    const results = useSelector((state) => state.music.results);
-    const hasSearchedRef = useRef(false); // Track if a search has been made
+    const batchResult = useSelector((state) => state.music.results);
+    const [musicPage, setMusicPage] = useState([]);
+    const pageConfig = useRef({ size: 0, lastIx: 0 })
+    const [loadingMore, setLoadingMore] = useState(false);
+    const hasSearchedRef = useRef(false);
 
-    // Fetch music only if results are empty and no search has been initiated (this is for when user navigates manually to the /musics route, without any search preference)
+    // Initial fetch when the component mounts
     useEffect(() => {
-        if (results.length === 0 && !hasSearchedRef.current) {
-            searchMusic('pop');
-            hasSearchedRef.current = true; // Mark that a search has been initiated
+        if (!hasSearchedRef.current) {
+            searchMusic('pop'); // Fetch the first 6 tracks
+            hasSearchedRef.current = true;
         }
-    }, [results.length, searchMusic]);
+    }, [searchMusic]);
+
+    useEffect(() => {
+        if (batchResult.length > 0) {
+            const firstPage = batchResult.slice(0, pageSize);
+            setMusicPage(firstPage);
+            pageConfig.current.lastIx = pageSize;
+            pageConfig.current.size = firstPage.length;
+        }
+    }, [batchResult.length]);
+
+
+    const loadMoreTracks = () => {
+        const { size, lastIx } = pageConfig.current;
+        if (loadingMore || lastIx >= batchResult.length) return;
+
+        setLoadingMore(true);
+
+        const nextSegment = batchResult.slice(lastIx, lastIx + pageSize); // Fetch the next segment of tracks
+        pageConfig.current.lastIx += nextSegment.length;
+
+        if (nextSegment.length === pageSize) {
+            setMusicPage((_) => [...nextSegment]);
+            pageConfig.current.size = nextSegment.length;
+        } else if (nextSegment.length > 0) {
+            setMusicPage((prevMusicPage) => [...prevMusicPage, ...nextSegment]);
+            pageConfig.current.size += nextSegment.length;
+        } else {
+
+        }
+
+        setLoadingMore(false);
+    };
+
+
+    const loadPrevTracks = () => {
+        const { size, lastIx } = pageConfig.current;
+
+
+        if (loadingMore || size === 0) return;
+
+        setLoadingMore(true);
+
+        const firstIdxInPage = lastIx - size;
+        const prevSegment = batchResult.slice(firstIdxInPage - pageSize, firstIdxInPage);
+
+        // Update the size and last index for pagination
+
+        pageConfig.current.lastIx -= size;
+        pageConfig.current.size = pageSize;
+
+
+        if (prevSegment.length > 0) {
+            setMusicPage(prevSegment); // Set the musicPage to the previous segment
+        }
+
+        setLoadingMore(false);
+    };
+
+
+    useEffect(() => {
+        const handleScroll = throttle(() => {
+            // Check if scrolled to the top
+            if (document.documentElement.scrollTop === 0) {
+                loadPrevTracks(); // Call the additional function when at the top
+            }
+            if (
+                window.innerHeight + document.documentElement.scrollTop >=
+                document.documentElement.offsetHeight - 10
+            ) {
+                loadMoreTracks();
+            }
+        }, 100);
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [batchResult]);
+
+
 
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Music List</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {results.map((music) => (
+                {musicPage.map((music) => (
                     <MusicCard key={music.trackId} music={music} />
                 ))}
             </div>
             {loading && <p>Loading...</p>}
+            {loadingMore && <p>Loading more tracks...</p>}
             {error && <p className="text-red-500">{error}</p>}
         </div>
     );
