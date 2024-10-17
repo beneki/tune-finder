68b03f73feb15c68332fcc3d51667096410392d3
Benjamin <benjamin@ekramiand@gmail.com>
2024-10-17 12:29:42 +0100

    add music list component

---
+++ src/components/MusicList/MusicList.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const MusicList = () => {
+    const results = useSelector((state) => state.music.searchResults);
+
+    return (
+        <div>
+            <h1>Music List</h1>
+            <ul>
+                {results.map((music) => (
+                    <li key={music.trackId}>
+                        <Link to={`/music/${music.trackId}`}>{music.trackName}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default MusicList;
