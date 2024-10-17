231761bcab4704f151f5b2b6ad3a1cef349a359d
Benjamin <benjamin@ekramiand@gmail.com>
2024-10-17 12:05:29 +0100

    define index export to centralize the components export

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Dashboard } from './Dashboard/Dashboard';
+export { default as MusicList } from './MusicList/MusicList';
+export { default as MusicDetail } from './MusicDetail/MusicDetail';
+export { default as SearchBar } from './SearchBar/SearchBar';
