1b341c6053e0e4da623d598fa5f7f3afd1360fdd
Benjamin <benjamin@ekramiand@gmail.com>
2024-10-17 12:06:33 +0100

    add router to avoid manually switching between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
-import './App.css'
-import SearchBar from './components/SearchBar/SeachBar'
-import Card from './components/Card/Card'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Dashboard } from './components';
+
+
 
 function App() {
-  const [isResultready, setResultReady] = useState(false)
-  const [res, setRes] = useState([])
-  const toggleResult = (res) => {
-    console.log('toggled')
-    setResultReady(prev => !prev)
-    setRes(res)
-  }
+
 
   return (
-    <>
-      {
-        isResultready
-          ? res.map(({ trackId, artistName, trackName, collectionName, artworkUrl100 }) =>
-            <Card
-              key={trackId}
-              artistName={artistName}
-              trackName={trackName}
-              album={collectionName}
-              albumImg={artworkUrl100}
-            />
-          )
-          : <SearchBar toggleResult={toggleResult} />
-      }
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/music" element={<MusicList />} />
+        <Route path="/music/:id" element={<MusicDetail />} />
+      </Routes>
+    </Router>
   )
 }
 
