52a659a56edd7e062e40ca7b36c89bad2fd6cefa
Benjamin <benjamin@ekramiand@gmail.com>
2024-10-17 13:10:32 +0100

    hadle default no-preference on /music route

--- src/components/MusicList/MusicList.jsx
+++ src/components/MusicList/MusicList.jsx
@@ -1,19 +1,31 @@
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
+import { MusicCard } from './../';
+import useMusicSearch from './../../hooks/useMusicSearch';
 
 const MusicList = () => {
+    const { searchMusic, loading, error } = useMusicSearch();
     const results = useSelector((state) => state.music.results);
+    const hasSearchedRef = useRef(false); // Track if a search has been made
+
+    // Fetch music only if results are empty and no search has been initiated
+    useEffect(() => {
+        if (results.length === 0 && !hasSearchedRef.current) {
+            searchMusic('pop');
+            hasSearchedRef.current = true; // Mark that a search has been initiated
+        }
+    }, [results.length, searchMusic]);
 
     return (
-        <div>
-            <h1>Music List</h1>
-            <ul>
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Music List</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {results.map((music) => (
-                    <li key={music.trackId}>
-                        <Link to={`/music/${music.trackId}`}>{music.trackName}</Link>
-                    </li>
+                    <MusicCard key={music.trackId} music={music} />
                 ))}
-            </ul>
+            </div>
+            {loading && <p>Loading...</p>}
+            {error && <p className="text-red-500">{error}</p>}
         </div>
     );
 };
